80ae9849695c3e34fd0f41107cd0d76609755547
kfirMizrahi <kfir.linkedin@gmail.com>
2024-07-25 08:00:46 +0300

    first commit

---
+++ user/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from models import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(username=data['username'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.to_dict()), 201
+
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.to_dict())
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
